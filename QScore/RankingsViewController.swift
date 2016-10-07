0ef6d4d2681951a25f9aca966a052994ba17e91b
Tom Clark <moltom26@gmail.com>
2016-10-07 14:33:49 -0500

    Initial Commit

---
+++ QScore/RankingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RankingsViewController.swift
+//  QScore
+//
+//  Created by Tom Clark on 10/6/16.
+//  Copyright © 2016 Tom Clark. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class RankingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var RankingsTableView: UITableView!
+    
+    
+    
+}
